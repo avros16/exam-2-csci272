d6b6085afaacee90c48f697ab45d3e492ef55ae1
avros16 <114757828+avros16@users.noreply.github.com>
2024-08-01 13:02:15 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@exam-2-csci272/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Employee {
+public:
+    string name;
+    int yearOfJoining;
+    string address;
+
+    Employee(string n, int y, string a) {
+        name = n;
+        yearOfJoining = y;
+        address = a;
+    }
+};
+
+int main() {
+    Employee emp1("Robert", 1994, "64C- WallsStreat");
+    Employee emp2("Sam", 2000, "68D- WallsStreat");
+    Employee emp3("John", 1999, "26B- WallsStreat");
+
+    cout << "Name\t\tYear of joining\t\tAddress" << endl;
+    cout << emp1.name << "\t\t" << emp1.yearOfJoining << "\t\t\t" << emp1.address << endl;
+    cout << emp2.name << "\t\t" << emp2.yearOfJoining << "\t\t\t" << emp2.address << endl;
+    cout << emp3.name << "\t\t" << emp3.yearOfJoining << "\t\t\t" << emp3.address << endl;
+
+    return 0;
+}
